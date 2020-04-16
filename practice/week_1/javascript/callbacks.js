55bab158a6f9880469265223be68a697c1710d0b
Eduardo <medatech@medasf.org>
2020-04-16 11:14:17 -0700

    Looking into Callback functions and how they work

--- practice/week_1/javascript/callbacks.js
+++ practice/week_1/javascript/callbacks.js
@@ -50,3 +50,42 @@ argumentPractice(true);
 
 console.log("hello");
 
+// CALLBACKS
+
+//$(document).ready( function () {} );
+
+// examples of methods, or functions that belong to objects.
+console.log();
+Math.random();
+
+function callBackReader(parameter) {
+
+    console.log("callBackReader has started.");
+
+    // console.log("Our only parameter was given the value: " + parameter);
+
+    if (typeof parameter === "function") {
+        console.log("The datatype of this value is a function.");
+
+        setTimeout(parameter, 3000);
+
+    } else {
+        console.log("The parameter is holding something else, should only be a function.");
+    }
+
+    console.log("callBackReader has ended.");
+}
+
+let mySpecialFunction = function () {
+    console.log("My special function ran!");
+}
+
+// mySpecialFunction actually holds a definition of a function, and we pass that definition to callBackReader, in callBackReader we store this function definition in the parameter called parameter. We then decide what we want to do with this definition inside of callBackReader.
+callBackReader(mySpecialFunction);
+
+// typeof keyword, returns a string that describes the datatype of the item after it.
+
+console.log(typeof "something");
+
+// Testing arrays is a little special compared to the typeof keyword
+console.log(Array.isArray(myArray));
