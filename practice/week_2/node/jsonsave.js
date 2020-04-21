78639198d6948d7c1451dfea419f0585d33d772f
Eduardo <medatech@medasf.org>
2020-04-21 11:05:55 -0700

    JSON Challenge

--- practice/week_2/node/jsonsave.js
+++ practice/week_2/node/jsonsave.js
@@ -1,6 +1,9 @@
+// Convert the following object into JSON and save that JSON file inside of the homework folder of YOUR repository.
+
+
 let objectToSave = {
     key1: "Some key",
     isTrue: false,
     someFunction: function () {console.log("hello");},
     totalAmount: 100900
-}
+};
