f161d039aa7b049b679c5f5b1e7fffd182123fda
Eduardo <medatech@medasf.org>
2020-04-30 11:32:15 -0700

    Finished our first HTTP conversation for FirstServer express practice project

--- practice/week_3/express/firstServer/index.js
+++ practice/week_3/express/firstServer/index.js
@@ -1,5 +1,6 @@
 // Including the Express package for our script.
 const express = require("express");
+const bodyParser = require("body-parser");
 
 // Run the Express server.
 const app = express();
@@ -15,10 +16,22 @@ http.listen(port);
 
 // Express Routes
 // express.static("./public_html") -> used to tell Express that it is a directory/folder.
+
+// Uses body-parser so we can read the JSON from the front-end (webpage).
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use("/", express.static("./public_html"));
 
 app.post("/submitAge", (request, response) => {
     console.log(request.body);
+    let canDrink = (request.body.age >= 21);
+
+    let dataToSendBackObject = {
+        "canDrink": canDrink
+    }
+
+    response.send(dataToSendBackObject);
 });
 
 // Signify Express Server is running.
